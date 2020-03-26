c7e2b8991c23c69e81fd4add524134e8555845c9
Nikolay427 <62320084+Nikolay427@users.noreply.github.com>
2020-03-26 21:37:50 +0700

    Add files via upload

---
+++ local storage/lesson 1/script.js
@@ -0,0 +1,9 @@
+﻿/*lesson*/
+let textarea = document.getElementById("textarea");
+textarea.addEventListener('blur',function(){
+	localStorage['value'] = textarea.value;
+
+});
+window.addEventListener('load', function(){
+	if (localStorage['value'] !== undefined) textarea.value = localStorage['value'];
+});
