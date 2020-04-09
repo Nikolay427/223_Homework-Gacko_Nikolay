c9f5d2ea5bbb04149733a191344697e4716892e5
Nikolay427 <62320084+Nikolay427@users.noreply.github.com>
2020-04-09 16:24:11 +0700

    Add files via upload

---
+++ local storage/lesson 4/script.js
@@ -0,0 +1,17 @@
+﻿//Задание 4
+let textpole1 = document.querySelector('#text1');
+let textpole2 = document.querySelector('#text2');
+let button1 = document.querySelector('#save');
+let button2 = document.querySelector('#show');
+let valueArray = [];
+
+button1.addEventListener('click', function (argument) {
+valueArray.push(textpole1.value);
+localStorage.setItem('text', valueArray);
+textpole1.value = '';
+});
+
+button2.addEventListener('click', function () {
+let perem = localStorage.getItem('text');
+textpole2.value = perem;
+});
