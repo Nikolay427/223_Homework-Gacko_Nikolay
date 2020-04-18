78bc2d0f99f002bc069a9ffd9e8db0ebff673e51
Nikolay427 <62320084+Nikolay427@users.noreply.github.com>
2020-04-18 17:18:36 +0700

    Add files via upload

---
+++ oop/lesson 67/rectangle.js
@@ -0,0 +1,23 @@
+﻿class Rectangle {
+constructor (width, height) {
+this.per= document.createElement('div');
+this.setWidth(width);
+this.setHeight(height);
+this.per.style.border = '5px solid blue';
+
+document.body.appendChild(this.per);
+}
+
+setWidth(width) {
+this.per.style.width = width + 'px';
+}
+getWidth(width) {
+return parseInt(this.per.style.width);
+}
+setHeight(height) {
+this.per.style.height = height + 'px';
+}
+getHeight(height) {
+return parseInt(this.per.style.height);
+}
+}
---
+++ oop/lesson 67/script.js
@@ -0,0 +1,8 @@
+﻿// Задание 3
+let rectangle = new Rectangle(500,500);
+
+console.log(rectangle.getWidth());
+rectangle.setWidth(500);
+
+console.log(rectangle.getHeight());
+rectangle.setHeight(500);
