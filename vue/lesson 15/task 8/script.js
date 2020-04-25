dffa530bb185b82a0eeca9217fdff72e3d39a0fb
Nikolay427 <62320084+Nikolay427@users.noreply.github.com>
2020-04-25 22:27:58 +0700

    Add files via upload

---
+++ vue/lesson 15/task 8/script.js
@@ -0,0 +1,11 @@
+﻿let app = new Vue({
+	el: '#app',
+	data: {
+		res: '22.06.1941',
+	},
+	filters: {
+		daymouthyear: function(value){
+			return value.split('-').reverse().join('.');
+		}
+	}
+});
